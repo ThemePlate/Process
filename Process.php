b040541a410143fbed49b06b0ae1e87dd409f6ff
kermage <genealyson.torcende@gmail.com>
2020-02-21 08:49:26 +0800

    make it a unique action

--- Process.php
+++ Process.php
@@ -11,24 +11,26 @@ namespace ThemePlate;
 
 class Process {
 
+	private $identifier;
 	private $callback_func;
 	private $callback_args;
 
 
 	public function __construct( $callback_func, $callback_args = null ) {
 
+		$this->identifier    = 'tpp_' . $callback_func;
 		$this->callback_func = $callback_func;
 		$this->callback_args = $callback_args;
 
-		add_action( 'wp_ajax_' . $this->callback_func, array( $this, 'handle' ) );
-		add_action( 'wp_ajax_nopriv_' . $this->callback_func, array( $this, 'handle' ) );
+		add_action( 'wp_ajax_' . $this->identifier, array( $this, 'handle' ) );
+		add_action( 'wp_ajax_nopriv_' . $this->identifier, array( $this, 'handle' ) );
 
 	}
 
 
 	public function handle() {
 
-		if ( wp_verify_nonce( $_REQUEST['nonce'], $this->callback_func ) ) {
+		if ( wp_verify_nonce( $_REQUEST['nonce'], $this->identifier ) ) {
 			call_user_func_array( $this->callback_func, $this->callback_args );
 		}
 
@@ -40,8 +42,8 @@ class Process {
 	public function dispatch() {
 
 		$query_args = array(
-			'action' => $this->callback_func,
-			'nonce'  => wp_create_nonce( $this->callback_func ),
+			'action' => $this->identifier,
+			'nonce'  => wp_create_nonce( $this->identifier ),
 		);
 		$post_url   = add_query_arg( $query_args, admin_url( 'admin-ajax.php' ) );
 		$post_args  = array(
