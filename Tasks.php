42987d7ef93da339c11c1f9ab989501e6685713b
kermage <genealyson.torcende@gmail.com>
2020-02-27 08:56:11 +0800

    ensure only one instance is running

--- Tasks.php
+++ Tasks.php
@@ -25,6 +25,11 @@ class Tasks {
 
 	public function runner( $identifier ) {
 
+		if ( $this->is_running() ) {
+			wp_die();
+		}
+
+		$this->lock();
 		$this->tasks = get_option( $identifier . '_tasks', array() );
 
 		foreach ( $this->tasks as $index => $task ) {
@@ -32,6 +37,7 @@ class Tasks {
 		}
 
 		delete_option( $identifier . '_tasks' );
+		$this->unlock();
 
 	}
 
@@ -47,6 +53,27 @@ class Tasks {
 	public function add( $callback_func, $callback_args = array() ) {
 
 		$this->tasks[] = compact( 'callback_func', 'callback_args' );
+
+	}
+
+
+	private function is_running() {
+
+		return get_option( $this->identifier . '_lock' );
+
+	}
+
+
+	private function lock() {
+
+		update_option( $this->identifier . '_lock', microtime(), false );
+
+	}
+
+
+	private function unlock() {
+
+		delete_option( $this->identifier . '_lock' );
 
 	}
 
